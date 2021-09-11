534d4c2e2b2d7e90c19d7c19bdd79027b8a51d98
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-11 13:45:14 -0700

    done with html and js code and beautify all the file

--- istriangle.js
+++ istriangle.js
@@ -1,24 +1,23 @@
 const inputTriangle = document.querySelectorAll(".input-triangle");
 const checkTriangle = document.querySelector("#check-triangle")
 const outputTriangle = document.querySelector("#output-triangle");
-function calculateSumOfAngles(a,b,c){
-  const  sumOfAngles = a+b+c;
+
+function calculateSumOfAngles(a, b, c) {
+  const sumOfAngles = a + b + c;
   return sumOfAngles;
-  
+
 }
-   
-
- function isTriangle(){
-    const sumOfAngles = calculateSumOfAngles(Number(inputTriangle[0].value),Number(inputTriangle[1].value),Number(inputTriangle[2].value));
-
-    if(sumOfAngles === 180){
-        outputTriangle.innerText = "yay,The angle from 🔺";
-    }else
-    {
-        outputTriangle.innerText= "oh oh! the angles don't from a triangle 😔 ";
-    }
- }
 
 
- checkTriangle.addEventListener("click",isTriangle);
-   
+function isTriangle() {
+  const sumOfAngles = calculateSumOfAngles(Number(inputTriangle[0].value), Number(inputTriangle[1].value), Number(inputTriangle[2].value));
+
+  if (sumOfAngles === 180) {
+    outputTriangle.innerText = "yay,The angle from 🔺";
+  } else {
+    outputTriangle.innerText = "oh oh! the angles don't from a triangle 😔 ";
+  }
+}
+
+
+checkTriangle.addEventListener("click", isTriangle);
