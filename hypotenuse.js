5ca3b4c0a2785d8c022de717dfef47aca706f537
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-20 10:11:13 -0700

    update hypoteneus

--- hypotenuse.js
+++ hypotenuse.js
@@ -4,15 +4,24 @@ const output = document.querySelector("#output");
 
 function totalSum(a, b) {
     const sumHypotenuse = (a * a) + (b * b)
-    return sumHypotenuse;
+    const ActualOutput = Math.sqrt(sumHypotenuse);
+    output.innerText = "your Hypotenuse is " + ActualOutput;
 }
 
 
 function Hypotenuse() {
 
-    const sumHypotenuse = totalSum(Number(sideTriangle[0].value), Number(sideTriangle[1].value))
-    const ActualOutput = Math.sqrt(sumHypotenuse);
-    output.innerText = "your Hypotenuse is " + ActualOutput;
+    var base = Number(sideTriangle[0].value);
+    var height = Number(sideTriangle[1].value);
+    if(base == "" || height == ""){
+        output.innerText = "please enter both the value"
+    }
+    else if(base < 0 || height < 0){
+        output.innerText = "please enter positive value"
+    }
+    else{
+        totalSum(base,height)
+    }
 
 }
 
