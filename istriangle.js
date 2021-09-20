f8da74cbbfe8c6fca0005961cd0d624fbc4e0658
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-20 09:59:24 -0700

    update istriangle js

--- istriangle.js
+++ istriangle.js
@@ -4,19 +4,29 @@ const outputTriangle = document.querySelector("#output-triangle");
 
 function calculateSumOfAngles(a, b, c) {
   const sumOfAngles = a + b + c;
-  return sumOfAngles;
-
-}
-
-
-function isTriangle() {
-  const sumOfAngles = calculateSumOfAngles(Number(inputTriangle[0].value), Number(inputTriangle[1].value), Number(inputTriangle[2].value));
-
   if (sumOfAngles === 180) {
     outputTriangle.innerText = "yay,The angle from 🔺";
   } else {
     outputTriangle.innerText = "oh oh! the angles don't from a triangle 😔 ";
   }
+
+}
+
+
+function isTriangle() {
+  var angle1 = Number(inputTriangle[0].value);
+  var angle2 = Number(inputTriangle[1].value);
+  var angle3 = Number(inputTriangle[2].value);
+  if(angle1=="" || angle2=="" || angle3==""){
+    outputTriangle.innerText = "please enter all the output";
+  }
+  else if(angle1 < 0 || angle2 < 0 || angle3 < 0){
+    outputTriangle.innerText = "Please enter positive value"
+  }
+  else{
+    calculateSumOfAngles(angle1,angle2,angle3);
+  }
+  
 }
 
 
